8ebb9924eb1abbf664651f7a4b50add5051c7886
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-08 09:57:13 +0300

    Hero area using Strapi data

--- src/components/Sections/Hero.jsx
+++ src/components/Sections/Hero.jsx
@@ -1,16 +1,21 @@
 import css from './Hero.module.css';
 import Button from '../UI/Button';
 import SocialLinks from '../UI/SocialLinks.style';
+import useStrapi from '../../hooks/useStrapi';
 
 export default function Hero() {
+  // panautoti useStrapi
+  const [heroData, isLoading] = useStrapi('/canvas-hero');
+  // console.log(heroData);
+  // supildyti reiksmes
   return (
     <section className={css.hero}>
       <div className={css['center-part']}>
-        <h1>Get the new Mop Dog</h1>
-        <p>You know you want it.</p>
+        <h1>{isLoading ? '...' : heroData.mainTitle}</h1>
+        <p>{heroData.subtitle}</p>
         <div className={css.controll}>
-          <Button>Shop men</Button>
-          <Button>Shop Woman</Button>
+          <Button>{heroData.button1}</Button>
+          <Button>{heroData.button2}</Button>
         </div>
       </div>
       <SocialLinks dash hero />
