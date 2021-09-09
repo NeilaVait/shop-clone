6399aa6bb59b009844f7a58cd947d9f4ae779524
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-09 09:24:35 +0300

    separate pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,74 +1,20 @@
-import { useRef, useEffect, useState } from 'react';
 import '../style/App.css';
 import 'font-awesome/css/font-awesome.css';
-import Hero from './Sections/Hero';
-import Collections from './Sections/Collections/Collections';
-import Shop from './Sections/Shop/Shop';
-import Blog from './Sections/Blog/Blog';
-import Cta from './Sections/CTA/Cta';
 import Layout from './Layout/Layout';
-import Shipping from './Sections/Shipping/Shipping';
 import { Switch, Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import BlogPage from '../pages/BlogPage';
+
 function App() {
-  const blogRef = useRef();
-  const asideRef = useRef();
-
-  const [asideStick, setAsideStick] = useState(false);
-
-  // useEffect(() => {
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, []);
-
-  function handleScroll() {
-    const asideTopDistance = asideRef.current.getBoundingClientRect().top;
-    const asideBottomDistance = asideRef.current.getBoundingClientRect().bottom;
-    const blogTopDistance = blogRef.current.getBoundingClientRect().top;
-    console.log({ asideTopDistance });
-    console.log({ asideBottomDistance });
-    console.log({ blogTopDistance });
-
-    // normali busena
-    // aside sicky
-    // nuimti nuo aside stiky esant zemiau
-
-    if (asideTopDistance < 85) {
-      console.log('stick');
-
-      if (asideBottomDistance > blogTopDistance) {
-        console.log('aside bottom reach NOT');
-        setAsideStick(false);
-        return;
-      }
-
-      setAsideStick(true);
-    } else {
-      // console.log('stick NOT');
-      // setAsideStick(false);
-    }
-  }
-
-  function btnHandler() {
-    console.log('blog top', blogRef.current.getBoundingClientRect().top);
-    console.log('aside', asideRef.current.getBoundingClientRect().top);
-  }
-
   return (
     <Layout>
       {/* <button onClick={btnHandler}>Get ref</button> */}
       <Switch>
         <Route path='/blog'>
-          <Blog />
+          <BlogPage />
         </Route>
         <Route path='/'>
-          <Hero />
-          <Collections />
-          <Shop asideStick={asideStick} ref={asideRef} />
-          <Blog ref={blogRef} qty='2' />
-          <Cta />
-          <Shipping />
+          <HomePage />
         </Route>
       </Switch>
     </Layout>
---
+++ src/pages/BlogPage.js
@@ -0,0 +1,8 @@
+import Blog from '../components/Sections/Blog/Blog';
+export default function BlogPage() {
+  return (
+    <div>
+      <Blog />
+    </div>
+  );
+}
